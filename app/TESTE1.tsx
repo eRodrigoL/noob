03181be88b2573a01d78a266a1bd38db05c71754
Emerson Rodrigo Lopes <e.rodrigo.l@outlook.com>
2024-11-12 22:03:17 -0300

    Corrigido o empecilho para digitar o apelido

--- app/TESTE1.tsx
+++ app/TESTE1.tsx
@@ -127,7 +127,7 @@ const RegisterUser: React.FC = () => {
         <Text style={styles.label}>Apelido:</Text>
         <TextInput
           style={styles.input}
-          value={nome}
+          value={apelido}
           placeholder="Apelido"
           onChangeText={setApelido}
         />
